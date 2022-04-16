f8e15db8087adceecdf06fb822ae2fa973f24e55
unknown <kamalpreetSingh0557@gmail.com>
2022-04-17 01:23:40 +0530

    FrontEnd UI page implemented

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -24,14 +24,39 @@ const http = require('http');
 
 //////////////////////////////////////////////////////////////////
 // SERVER
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
-// const dataObj = JSON.parse(data);
+const dataObj = JSON.parse(data); // Ye object mein convert kr deta hai
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if(!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     // routing in NodeJS
     if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the Overview');
+        res.writeHead(200, {'Content-type' : 'text/html'})
+        // map => objects pe lgta hai is liye dataObj not data.map()
+        //placeholders ko [in template-product] data [in data.json] se replace kr dia hai
+        //and usko [HTML format] se [ek string] mein convert kr dia hai using [join(' ')]
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        //console.log(output);
+        res.end(output);
     } else if(pathName === '/product'){
         res.end('This is the Product');
     }else if(pathName === '/api'){
---
+++ 1-node-farm/starter/templates/things.md
@@ -0,0 +1,1 @@
+https://stackoverflow.com/questions/33439812/what-is-the-meaning-of-tags-in-html
