22c5fcb5f0aeef8f897137ec82491591a02e8ccc
unknown <kamalpreetSingh0557@gmail.com>
2022-05-10 16:55:20 +0530

    SignUp users implemented with JWT

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,15 +1,28 @@
 const User = require('./../models/userModel');
+const jwt = require('jsonwebtoken');
+
 
 exports.signUp = async(req, res, next) => {
     try{
-        const newUser = await User.create(req.body);
+        const newUser = await User.create({
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.password,
+            passwordConfirm : req.body.passwordConfirm
+        });
+
+        const token = jwt.sign({id : newUser._id}, process.env.JWT_SECRET, {
+            expiresIn : process.env.JWT_EXPIRES_IN
+        });
+
         res.status(201).json({ // 201 for creation
             status : "success",
             message : "New user created",
+            token,
             data : {
                 newUser
             }
-        })
+        });
     }
     catch(err){
         res.status(400).json({
