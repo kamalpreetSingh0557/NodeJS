72c9d61e63bde98abfb2364d6b58e0ca1165723f
unknown <kamalpreetSingh0557@gmail.com>
2022-05-10 18:16:30 +0530

    Login Step 1

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,7 +1,6 @@
 const User = require('./../models/userModel');
 const jwt = require('jsonwebtoken');
 
-
 exports.signUp = async(req, res, next) => {
     try{
         const newUser = await User.create({
@@ -31,3 +30,30 @@ exports.signUp = async(req, res, next) => {
         });
     }
 };
+
+exports.login = (req, res, next) => {
+    const {email, password} = req.body;
+    // 1) Check if user entered email and password
+    if(!email || !password){
+        return res.status(400).json({
+            status : 'fail',
+            message : 'Please enter email or password'
+        });
+    }
+    try{
+    // 2) Check is user exist and password is correct
+        const user = User.findOne({email});
+    // 3) If everything is ok, send token to client
+        const token = " ";
+        res.status(200).json({
+            status : "success",
+                message : "User loggedIn",
+                token
+        })
+    }catch(err){
+        return res.status(400).json({
+            status : 'fail',
+            message : err
+        });
+    }
+}
